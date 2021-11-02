08a0d36a39d21a99db6e25d44a33893b76b957a1
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2021-11-03 04:00:23 +0600

    add file

--- generator/views.py
+++ generator/views.py
@@ -1,11 +1,20 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse 
+import random
 
 def home(request):
     return render(request, 'generator/index.html')
 
 def password(request):
-    return render(request,'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz0123456789+-.')
+    length = 16
+    thepassword = ''
+    for x in range(length) :
+        thepassword +=  random.choice(characters)
+
+
+    return render(request,'generator/password.html',{'password' : thepassword })
     
     
