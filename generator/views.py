f48af3f78326cd3a45414ba93949d3f8ccdfeb07
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2021-11-05 01:45:06 +0600

    added code

--- generator/views.py
+++ generator/views.py
@@ -6,10 +6,19 @@ import random
 def home(request):
     return render(request, 'generator/index.html')
 
+
+def about(request):
+    return render(request, 'generator/about.html')
 def password(request):
 
-    characters = list('abcdefghijklmnopqrstuvwxyz0123456789+-.')
-    length = 16
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('.@#$%^&*'))
+    if request.GET.get('number'):
+        characters.extend(list('0123456789'))
+    length = int(request.GET.get('length',12))
     thepassword = ''
     for x in range(length) :
         thepassword +=  random.choice(characters)
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -17,6 +17,7 @@ from generator import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home),
-    path('passwordgenarate',views.password, name='password')
+    path('', views.home, name= 'home'),
+    path('password/',views.password, name='password'),
+    path('about/',views.about, name='about')
 ]
